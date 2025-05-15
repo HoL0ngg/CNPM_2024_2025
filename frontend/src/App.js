f0b043545cecc71a0a9508533f7679a76b071533
Trong Nghia <nghiatrong060505@gmail.com>
2025-05-15 21:44:23 +0700

    sua

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
-import AppRoutes from "./routes/AppRoutes";
-import { ToastContainer } from "react-toastify";
+import FoodMenu from "./components/FoodMenu";
+import Login from "./components/Login"; // 👈 thêm dòng này
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const [user, setUser] = useState(null);
 
   const handleLoginSuccess = (userData) => {
-    setUser(userData);
+    console.log("Data nhận được:", userData); // 👈 Kiểm tra tại đây
+    setUser(userData); // Lưu user sau khi đăng nhập thành công
   };
 
   return (
     <div className="App">
-      <AppRoutes user={user} onLoginSuccess={handleLoginSuccess} />
+      {!user ? (
+        <Login onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <>
+          {/* <h3>Chào {user.username}</h3> */}
+          <FoodMenu />
+        </>
+      )}
       <ToastContainer />
     </div>
   );
 }
-
 export default App;
--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,36 +1,36 @@
-// src/routes/AppRoutes.js
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../components/Login";
-import FoodMenu from "../components/FoodMenu";
-import UserManagement from "../components/pages/admin/UserManagement";
-// import FoodManagement from "../pages/staff/FoodManagement";
+// // src/routes/AppRoutes.js
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Login from "../components/Login";
+// import FoodMenu from "../components/FoodMenu";
+// import UserManagement from "../components/pages/admin/UserManagement";
+// // import FoodManagement from "../pages/staff/FoodManagement";
 
-const AppRoutes = ({ user, onLoginSuccess }) => {
-  if (!user)
-    return (
-      <Routes>
-        <Route path="/" element={<Login onLoginSuccess={onLoginSuccess} />} />
-        <Route
-          path="/login"
-          element={<Login onLoginSuccess={onLoginSuccess} />}
-        />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    );
+// const AppRoutes = ({ user, onLoginSuccess }) => {
+//   if (!user)
+//     return (
+//       <Routes>
+//         <Route path="/" element={<Login onLoginSuccess={onLoginSuccess} />} />
+//         <Route
+//           path="/login"
+//           element={<Login onLoginSuccess={onLoginSuccess} />}
+//         />
+//         <Route path="*" element={<Navigate to="/login" />} />
+//       </Routes>
+//     );
 
-  return (
-    <Routes>
-      {user.role === "admin" && (
-        <Route path="/admin" element={<UserManagement />} />
-      )}
-      {/* {user.role === "staff" && (
-        <Route path="/staff" element={<FoodManagement />} />
-      )} */}
-      {user.role === "user" && <Route path="/user" element={<FoodMenu />} />}
-      {/* Chuyển hướng nếu không khớp */}
-      <Route path="*" element={<Navigate to={`/${user.role}`} />} />
-    </Routes>
-  );
-};
+//   return (
+//     <Routes>
+//       {user.role === "admin" && (
+//         <Route path="/admin" element={<UserManagement />} />
+//       )}
+//       {/* {user.role === "staff" && (
+//         <Route path="/staff" element={<FoodManagement />} />
+//       )} */}
+//       {user.role === "user" && <Route path="/user" element={<FoodMenu />} />}
+//       {/* Chuyển hướng nếu không khớp */}
+//       <Route path="*" element={<Navigate to={`/${user.role}`} />} />
+//     </Routes>
+//   );
+// };
 
-export default AppRoutes;
+// export default AppRoutes;
